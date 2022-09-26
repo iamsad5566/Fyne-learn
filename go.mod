52e63815ffb207dc1b242bc0a9ab7c26b50b529c
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-09-26 22:35:21 +0800

    update: first push

---
+++ go.mod
@@ -0,0 +1,20 @@
+module go-pyscrape-gui
+
+go 1.18
+
+require (
+	gioui.org v0.0.0-20220924144830-24eb1a4fc5fd
+	gioui.org/x v0.0.0-20220919203322-ce11c01a7117
+)
+
+require (
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	github.com/benoitkugler/textlayout v0.1.3 // indirect
+	github.com/gioui/uax v0.2.1-0.20220819135011-cda973fac06d // indirect
+	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	go func() {
+		// create new window
+		w := app.NewWindow(
+			app.Title("Egg timer"),
+			app.Size(unit.Dp(1400), unit.Dp(800)),
+		)
+
+		// ops are the operations from the UI
+		var ops op.Ops
+
+		// startButton is a clickable widget
+		var startButton widget.Clickable
+
+		// th defnes the material design style
+		th := material.NewTheme(gofont.Collection())
+
+		// listen for events in the window.
+		for e := range w.Events() {
+
+			// detect what type of event
+			switch e := e.(type) {
+
+			// this is sent when the application should re-render.
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				// Let's try out the flexbox layout concept:
+				layout.Flex{
+					// Vertical alignment, from top to bottom
+					Axis: layout.Vertical,
+					// Empty space is left at the start, i.e. at the top
+					Spacing: layout.SpaceStart,
+				}.Layout(gtx,
+					// We insert two rigid elements:
+					// First a button ...
+					layout.Rigid(
+						func(gtx layout.Context) layout.Dimensions {
+							btn := material.Button(th, &startButton, "Start")
+							return btn.Layout(gtx)
+						},
+					),
+					// ... then an empty spacer
+					layout.Rigid(
+						// The height of the spacer is 25 Device independent pixels
+						layout.Spacer{Height: unit.Dp(25)}.Layout,
+					),
+				)
+				e.Frame(gtx.Ops)
+			}
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
