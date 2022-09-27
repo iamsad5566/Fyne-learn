dd9449910ee12cde0540a89dbb739f79ff725c08
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-09-28 07:34:05 +0800

    update: update 2022/09/28

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
 	"os"
+	"time"
 
 	"gioui.org/app"
 	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
+
+var progress float32
+var progressIncrementer chan float32
+var boiling bool
+
+type Point struct {
+	X, Y float32
+}
+
+type Rectangle struct {
+	Min, Max Point
+}
 
 func main() {
 	go func() {
 		// create new window
 		w := app.NewWindow(
 			app.Title("Egg timer"),
-			app.Size(unit.Dp(1400), unit.Dp(800)),
+			app.Size(unit.Dp(400), unit.Dp(600)),
 		)
 
 		// ops are the operations from the UI
@@ -30,6 +47,14 @@ func main() {
 
 		// th defnes the material design style
 		th := material.NewTheme(gofont.Collection())
+
+		progressIncrementer = make(chan float32)
+		go func() {
+			for {
+				time.Sleep(time.Second / 25)
+				progressIncrementer <- 0.004
+			}
+		}()
 
 		if err := draw(w, ops, startButton, th); err != nil {
 			log.Fatal(err)
@@ -44,53 +69,91 @@ func draw(w *app.Window, ops op.Ops, startButton widget.Clickable, th *material.
 	type C = layout.Context
 	type D = layout.Dimensions
 	// listen for events in the window.
-	for e := range w.Events() {
+	for {
+		select {
+		case e := <-w.Events():
+			switch e := e.(type) {
+			// this is sent when the application should re-render.
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
 
-		// detect what type of event
-		switch e := e.(type) {
+				if startButton.Clicked() {
+					boiling = !boiling
+				}
 
-		// this is sent when the application should re-render.
-		case system.FrameEvent:
-			gtx := layout.NewContext(&ops, e)
-			// Let's try out the flexbox layout concept:
-			layout.Flex{
-				// Vertical alignment, from top to bottom
-				Axis: layout.Vertical,
-				// Empty space is left at the start, i.e. at the top
-				Spacing: layout.SpaceStart,
-			}.Layout(gtx,
-				// We insert two rigid elements:
-				// First a button ...
-				layout.Rigid(
-					func(gtx C) D {
-						margins := layout.Inset{
-							Top:    unit.Dp(25),
-							Bottom: unit.Dp(25),
-							Left:   unit.Dp(25),
-							Right:  unit.Dp(25),
-						}
+				// Let's try out the flexbox layout concept:
+				layout.Flex{
+					// Vertical alignment, from top to bottom
+					Axis: layout.Vertical,
+					// Empty space is left at the start, i.e. at the top
+					Spacing: layout.SpaceStart,
+				}.Layout(gtx,
+					// We insert two rigid elements:
+					layout.Rigid(
+						func(gtx C) D {
+							circle := clip.Ellipse{
+								// Hard coding the x coordinate. Try resizing the window
+								// Min: image.Pt(80, 0),
+								// Max: image.Pt(320, 240),
+								// Soft coding the x coordinate. Try resizing the window
+								Min: image.Pt(gtx.Constraints.Max.X/2-300, 0),
+								Max: image.Pt(gtx.Constraints.Max.X/2+300, 600),
+							}.Op(gtx.Ops)
+							color := color.NRGBA{R: 200, A: 255}
+							paint.FillShape(gtx.Ops, color, circle)
+							d := image.Point{Y: 800}
+							return layout.Dimensions{Size: d}
+						},
+					),
 
-						return margins.Layout(gtx,
-							func(gtx layout.Context) D {
-								btn := material.Button(th, &startButton, "Start")
-								return btn.Layout(gtx)
-							},
-						)
-					},
-				),
+					layout.Rigid(
+						func(gtx C) D {
+							bar := material.ProgressBar(th, progress)
+							return bar.Layout(gtx)
+						},
+					),
+					// First a button ...
+					layout.Rigid(
+						func(gtx C) D {
+							margins := layout.Inset{
+								Top:    unit.Dp(25),
+								Bottom: unit.Dp(25),
+								Left:   unit.Dp(25),
+								Right:  unit.Dp(25),
+							}
 
-				// ... then an empty spacer
-				layout.Rigid(
-					// The height of the spacer is 25 Device independent pixels
-					layout.Spacer{Height: unit.Dp(25)}.Layout,
-				),
-			)
-			e.Frame(gtx.Ops)
+							return margins.Layout(gtx,
+								func(gtx layout.Context) D {
+									text := ""
+									if boiling {
+										text = "Stop"
+									} else {
+										text = "Start"
+									}
+									btn := material.Button(th, &startButton, text)
+									return btn.Layout(gtx)
+								},
+							)
+						},
+					),
 
-		case system.DestroyEvent:
-			return e.Err
+					// ... then an empty spacer
+					layout.Rigid(
+						// The height of the spacer is 25 Device independent pixels
+						layout.Spacer{Height: unit.Dp(25)}.Layout,
+					),
+				)
+				e.Frame(gtx.Ops)
+
+			case system.DestroyEvent:
+				return e.Err
+			}
+
+		case p := <-progressIncrementer:
+			if boiling && progress < 1 {
+				progress += p
+				w.Invalidate()
+			}
 		}
-
 	}
-	return nil
 }
