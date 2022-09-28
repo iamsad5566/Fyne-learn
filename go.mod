f93b670a24b52bb50c0544cbd2e9cd715a0c9efa
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-09-28 23:23:13 +0800

    update: learn

--- go.mod
+++ go.mod
@@ -2,19 +2,34 @@ module go-pyscrape-gui
 
 go 1.18
 
-require (
-	gioui.org v0.0.0-20220924144830-24eb1a4fc5fd
-	gioui.org/x v0.0.0-20220919203322-ce11c01a7117
-)
+require fyne.io/fyne/v2 v2.2.3
 
 require (
-	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
-	gioui.org/shader v1.0.6 // indirect
-	github.com/benoitkugler/textlayout v0.1.3 // indirect
-	github.com/gioui/uax v0.2.1-0.20220819135011-cda973fac06d // indirect
-	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
-	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
-	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
-	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	fyne.io/fyne v1.4.3 // indirect
+	fyne.io/systray v1.10.1-0.20220621085403-9a2652634e93 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
+	github.com/fyne-io/glfw-js v0.0.0-20220120001248-ee7290d23504 // indirect
+	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	github.com/yuin/goldmark v1.4.0 // indirect
+	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
+	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
--- main.go
+++ main.go
@@ -1,237 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"image"
-	"image/color"
-	"log"
-	"math"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"gioui.org/app"
-	"gioui.org/f32"
-	"gioui.org/font/gofont"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
-	"gioui.org/text"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
-var boilDurationInput widget.Editor
-var boilDuration float32
-
-var progress float32
-var progressIncrementer chan float32
-var boiling bool
-
-type Point struct {
-	X, Y float32
-}
-
-type Rectangle struct {
-	Min, Max Point
-}
-
 func main() {
-	go func() {
-		// create new window
-		w := app.NewWindow(
-			app.Title("Egg timer"),
-			app.Size(unit.Dp(400), unit.Dp(600)),
-		)
+	a := app.New()
+	w := a.NewWindow("Hello World")
 
-		// ops are the operations from the UI
-		var ops op.Ops
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.Show()
+	w.SetMaster()
 
-		// startButton is a clickable widget
-		var startButton widget.Clickable
+	w2 := a.NewWindow("Larger")
+	w2.SetContent(widget.NewLabel("More content"))
+	w2.Resize(fyne.NewSize(100, 100))
+	w2.Show()
+	w2.SetContent(widget.NewButton("Open new", func() {
+		w3 := a.NewWindow("Third")
+		w3.SetContent(widget.NewLabel("Third"))
+		w3.Show()
+	}))
 
-		// th defnes the material design style
-		th := material.NewTheme(gofont.Collection())
-
-		progressIncrementer = make(chan float32)
-		go func() {
-			for {
-				time.Sleep(time.Second / 25)
-				progressIncrementer <- 0.004
-			}
-		}()
-
-		if err := draw(w, ops, startButton, th); err != nil {
-			log.Fatal(err)
-		}
-
-		os.Exit(0)
-	}()
-	app.Main()
-}
-
-func draw(w *app.Window, ops op.Ops, startButton widget.Clickable, th *material.Theme) error {
-	type C = layout.Context
-	type D = layout.Dimensions
-	// listen for events in the window.
-	for {
-		select {
-		case e := <-w.Events():
-			switch e := e.(type) {
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				gtx := layout.NewContext(&ops, e)
-
-				if startButton.Clicked() {
-					boiling = !boiling
-
-					// Read from the input box
-					inputString := boilDurationInput.Text()
-					inputString = strings.TrimSpace(inputString)
-					inputFloat, _ := strconv.ParseFloat(inputString, 32)
-					boilDuration = float32(inputFloat)
-					boilDuration = boilDuration / (1 - progress)
-				}
-
-				if boiling && progress < 1 {
-					boilRemain := (1 - progress) * boilDuration
-					// Format to 1 decimal.
-					inputStr := fmt.Sprintf("%.1f", math.Round(float64(boilRemain)*10)/10)
-					// Update the text in the inputbox
-					boilDurationInput.SetText(inputStr)
-				}
-
-				// Let's try out the flexbox layout concept:
-				layout.Flex{
-					// Vertical alignment, from top to bottom
-					Axis: layout.Vertical,
-					// Empty space is left at the start, i.e. at the top
-					Spacing: layout.SpaceStart,
-				}.Layout(gtx,
-					layout.Rigid(
-						func(gtx C) D {
-							// Draw a custom path, shaped like an egg
-							var eggPath clip.Path
-							op.Offset(image.Pt(gtx.Dp(200), gtx.Dp(150))).Add(gtx.Ops)
-							eggPath.Begin(gtx.Ops)
-							// Rotate from 0 to 360 degrees
-							for deg := 0.0; deg <= 360; deg++ {
-
-								// Egg math (really) at this brilliant site. Thanks!
-								// https://observablehq.com/@toja/egg-curve
-								// Convert degrees to radians
-								rad := deg / 360 * 2 * math.Pi
-								// Trig gives the distance in X and Y direction
-								cosT := math.Cos(rad)
-								sinT := math.Sin(rad)
-								// Constants to define the eggshape
-								a := 110.0
-								b := 150.0
-								d := 20.0
-								// The x/y coordinates
-								x := a * cosT
-								y := -(math.Sqrt(b*b-d*d*cosT*cosT) + d*sinT) * sinT
-								// Finally the point on the outline
-								p := f32.Pt(float32(x), float32(y))
-								// Draw the line to this point
-								eggPath.LineTo(p)
-							}
-							// Close the path
-							eggPath.Close()
-
-							// Get hold of the actual clip
-							eggArea := clip.Outline{Path: eggPath.End()}.Op()
-
-							// Fill the shape
-							// color := color.NRGBA{R: 255, G: 239, B: 174, A: 255}
-							color := color.NRGBA{R: 255, G: uint8(239 * (1 - progress)), B: uint8(174 * (1 - progress)), A: 255}
-							paint.FillShape(gtx.Ops, color, eggArea)
-
-							d := image.Point{Y: 335}
-							return layout.Dimensions{Size: d}
-						},
-					),
-
-					layout.Rigid(
-						func(gtx C) D {
-							ed := material.Editor(th, &boilDurationInput, "sec")
-							// Define characteristics of the input box
-							boilDurationInput.SingleLine = true
-							boilDurationInput.Alignment = text.Middle
-							// Define insets ...
-							margins := layout.Inset{
-								Top:    unit.Dp(0),
-								Right:  unit.Dp(170),
-								Bottom: unit.Dp(40),
-								Left:   unit.Dp(170),
-							}
-							// ... and borders ...
-							border := widget.Border{
-								Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 255},
-								CornerRadius: unit.Dp(3),
-								Width:        unit.Dp(2),
-							}
-							// ... before laying it out, one inside the other
-							return margins.Layout(gtx,
-								func(gtx C) D {
-									return border.Layout(gtx, ed.Layout)
-								},
-							)
-						},
-					),
-
-					layout.Rigid(
-						func(gtx C) D {
-							bar := material.ProgressBar(th, progress)
-							return bar.Layout(gtx)
-						},
-					),
-					// First a button ...
-					layout.Rigid(
-						func(gtx C) D {
-							margins := layout.Inset{
-								Top:    unit.Dp(25),
-								Bottom: unit.Dp(25),
-								Left:   unit.Dp(25),
-								Right:  unit.Dp(25),
-							}
-
-							return margins.Layout(gtx,
-								func(gtx layout.Context) D {
-									text := ""
-									if boiling {
-										text = "Stop"
-									} else {
-										text = "Start"
-									}
-									btn := material.Button(th, &startButton, text)
-									return btn.Layout(gtx)
-								},
-							)
-						},
-					),
-
-					// ... then an empty spacer
-					layout.Rigid(
-						// The height of the spacer is 25 Device independent pixels
-						layout.Spacer{Height: unit.Dp(25)}.Layout,
-					),
-				)
-				e.Frame(gtx.Ops)
-
-			case system.DestroyEvent:
-				return e.Err
-			}
-
-		case p := <-progressIncrementer:
-			if boiling && progress < 1 {
-				progress += p
-				w.Invalidate()
-			}
-		}
-	}
+	a.Run()
 }
