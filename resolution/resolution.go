324395433c7495f37926428b53341f8fd39238ea
Yen-Kuang Chen <nf8964p5566@gmail.com>
2022-09-29 19:06:20 +0800

    cache

--- resolution/resolution.go
+++ resolution/resolution.go
@@ -1,14 +1,33 @@
 package resolution
 
-var width int = 400
-var height int = 600
+import (
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+var width float32 = 400
+var height float32 = 600
 
 func Resolution() {
-	// app := app.New()
-	// window := app.NewWindow("resolution")
-	// window.Resize(fyne.NewSize(width, height))
-	// wInput := widget.NewEntry()
-	// wInput.Set
+	app := app.New()
+	window := app.NewWindow("resolution")
+	window.Resize(fyne.NewSize(width, height))
+	wInput := widget.NewEntry()
+	wInput.SetPlaceHolder("width")
+	hInput := widget.NewEntry()
+	hInput.SetPlaceHolder("height")
 
-	// window.ShowAndRun()
+	content := container.NewVBox(wInput, hInput, widget.NewButton("Save", func() {
+		w, _ := strconv.ParseFloat(wInput.Text, 32)
+		h, _ := strconv.ParseFloat(hInput.Text, 32)
+		width = float32(w)
+		height = float32(h)
+		window.Resize(fyne.NewSize(width, height))
+	}))
+	window.SetContent(content)
+	window.ShowAndRun()
 }
